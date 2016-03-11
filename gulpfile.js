804696a36bece4a6aed658e9c37a4eddb26cad52
ChrisBurgin <chrisburgin95@gmail.com>
2016-03-11 12:02:43 -0500

    Basic STart

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+//Package Imports
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    stylus = require('gulp-stylus'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps');
+
+//run 'gulp stylus'
+gulp.task('stylus', function() {
+    return gulp.src('src/main.styl')
+    .pipe(stylus())
+    .pipe(postcss([ autoprefixer({ browsers: ['> 5%'] }) ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
+});
+
+// run 'gulp watch'
+gulp.task('watch', function() {
+    gulp.watch('src/**/**.styl', ['stylus']);
+});
+
+// Default gulp task that runs when you 'gulp'
+gulp.task('default', ['stylus']);
